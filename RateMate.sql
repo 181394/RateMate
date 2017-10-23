762cd0981b4889c2f6122ac5c6e7d56869cc74c9
h181204 <fprytz@gmail.com>
2017-10-23 15:40:18 +0200

    sql kode

--- RateMate.sql
+++ RateMate.sql
@@ -10,6 +10,7 @@ PRIMARY KEY(emnekode));
 CREATE TABLE IF NOT EXISTS ratemate.foreleser (
 ansattnr 		VARCHAR(6)	NOT NULL,
 navn 			VARCHAR(40),
+passord			VARCHAR(50) NOT NULL,
 fagliste		VARCHAR(15),
 PRIMARY KEY(id),
 FOREIGN KEY (fagliste) REFERENCES (ratemate.fag(emnekode));
@@ -22,5 +23,17 @@ rating			INTEGER, check rating >= 0 and rating <= 2,
 PRIMARY KEY (studentnr),
 FOREIGN KEY (ratemate.fag(emnekode));
 
-
+CREATE TABLE IF NOT EXISTS ratemate.forelesning (
+id 			INTEGER NOT NULL,
+dato		VARCHAR(10),
+kl_start	VARCHAR(10),
+kl_slutt	VARCHAR(10),
+fag			VARCHAR(15),
+foreleser	VARCHAR(6),
+bra			INTEGER,
+middels 	INTEGER,
+daarlig		INTEGER,
+PRIMARY KEY(id),
+FOREIGN KEY (fag) REFERENCES (ratemate.fag(emnekode),
+FOREIGN KEY (foreleser) REFERENCES (ratemate.foreleser(ansattnr));
 

20a765b80468279607bb03b050792cd1e5138343
Preben Haukebøe <181394@stud.hvl.no>
2017-10-25 13:00:01 +0200

    stemmeservlet + stem.jsp

---
+++ WebContent/stem.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Stem</title>
+</head>
+<body>
+<h1>Stem på forelesning</h1>
+<form action="Stem" method="post">
+<input type="radio" name="stemme" value="0" checked="checked"/>Bra
+<input type="radio" name="stemme" value="1"/>Middels
+<input type="radio" name="stemme" value="2"/>Dårlig<br>
+<input type="submit" value="Stem"/>
+</form>
+</body>
+</html>
---
+++ src/no/hib/dat104/controller/StemmeServlet.java
@@ -0,0 +1,43 @@
+package no.hib.dat104.controller;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import no.hib.dat104.model.DBKom;
+import no.hib.dat104.model.Student;
+import no.hib.dat104.utils.SessionUtil;
+
+/**
+ * Servlet implementation class StemmeServlet
+ */
+@WebServlet("/Stem")
+public class StemmeServlet extends HttpServlet {
+	@EJB
+	DBKom dbk;
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Integer ny = Integer.parseInt(request.getParameter("stemme"));
+		Student stud = dbk.finnStudent(SessionUtil.isInnloggetSom(request));
+		Integer gammel = stud.getRating();
+		stud.setRating(ny);
+		dbk.UpdateLive(gammel, ny);
+		dbk.UpdateStudent(stud);
+		
+		response.sendRedirect("Stem");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		if (!SessionUtil.isInnlogget(request)) {
+			response.sendRedirect("Login");
+		}
+		request.getRequestDispatcher("WEB-INF/stem.jsp");
+	}
+}

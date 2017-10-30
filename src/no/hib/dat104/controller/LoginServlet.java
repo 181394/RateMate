f7cd3b51f358c0be3e19c30895a2777046e25147
h181186 <amanda.soreide@gmail.com>
2017-10-30 16:16:45 +0100

    Testepush

---
+++ src/no/hib/dat104/controller/LoginServlet.java
@@ -0,0 +1,50 @@
+package no.hib.dat104.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import no.hib.dat104.utils.SessionUtil;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(SessionUtil.isInnloggetStudent()) {
+			request.getRequestDispatcher("WEB-INF/stem.jsp").forward(request, response);
+		}
+		else if(SessionUtil.isInnloggetForeleser()){
+			request.getRequestDispatcher("WEB-INF/homeForeleser.jsp").forward(request, response);
+		}
+		else {
+			request.getRequestDispatcher("WEB-INF/Login.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
